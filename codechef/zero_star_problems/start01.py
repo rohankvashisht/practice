51cec5a47c4567f692a89d5b21bd8949ff1e93c9
r0hAn <13715543+rohankvashisht@users.noreply.github.com>
2022-03-12 09:39:37 +0530

    Create start01.py

---
+++ codechef/zero_star_problems/start01.py
@@ -0,0 +1,25 @@
+# Problem Name : Number Mirror
+
+"""
+Problem Statement :
+Write a program that accepts a number, 'n', and outputs the same.
+
+Input :
+The only line contains a single integer.
+
+Output :
+Output the answer in a single line.
+
+Constraints :
+0 ≤ n ≤ 105
+
+Sample Input:
+123
+
+Sample Output:
+123
+"""
+
+# cook your dish here
+x = int(input())
+print(x)
